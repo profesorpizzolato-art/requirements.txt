6610e3d015a931b288c9dd083117b2b32a930363
profesorpizzolato-art <profesorpizzolato@gmail.com>
2026-03-05 15:59:23 -0300

    Create simulador.py

---
+++ simulador.py
@@ -0,0 +1,24 @@
+MENFA_SIMULADOR/
+│
+├── simulador.py
+├── motor_fisico.py
+├── interfaz_cabina.py
+│
+├── assets/
+│   └── logo_menfa.png
+import sys
+from PyQt6.QtWidgets import QApplication
+from interfaz_cabina import CabinaPerforador
+
+def main():
+
+    app = QApplication(sys.argv)
+
+    ventana = CabinaPerforador()
+    ventana.show()
+
+    sys.exit(app.exec())
+
+if __name__ == "__main__":
+    main()
+  
